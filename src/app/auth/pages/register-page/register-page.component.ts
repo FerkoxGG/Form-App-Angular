eb47ae4a9ff562e7bfbbb3554deb3b2219c55787
Ferkox <fernandogonzalezgaete@gmail.com>
2024-02-14 22:27:41 -0300

    Implement reactive forms in the registration page and add unit tests

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { cantBeStrider } from "../../../shared/validators/validators";
 
 @Component({
-  templateUrl: './register-page.component.html',
-  styles: [
-  ]
+  templateUrl: "./register-page.component.html",
+  styles: []
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ["", [Validators.required]],
+    email: ["", [Validators.required, Validators.email]],
+    username: ["", [Validators.required, cantBeStrider]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ["", [Validators.required]]
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {
+  // TODO: obtener validación desde un servicio
+
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
